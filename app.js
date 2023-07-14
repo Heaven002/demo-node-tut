275b7626d1e2634049daf4f8fa1f2aaa9435ab65
Heaven002 <heavenatlast66@gmail.com>
2023-07-14 09:18:57 +0100

    my learning about event loop

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-//npm - global command, comes with node
-//npm --cersion
+const {readFile} = require('fs');
 
-//local dependency - use it only in this particular project
-//npm i <packageName>
-
-//global dependency - use it in any project
-//npm install -g <packageName>
-//sudo npm install -g <packageName> (mac)
-
-//package.json - manifest file [stores important info about project/package]
-//manual approach(create package.son in the root, create properties etc)
-//npm init (step by step, press enter to skip)
-//npm init -y (everything default)
-const _ = require ('lodash');
-
-const items = [1, [2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
+readFile('./content/first.txt', 'utf8', (err,data ) => {
+ if (err) {
+  return
+ }else{
+  console.log(data)
+ }
+})
